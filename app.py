7280feee7d73dd1b81677873e254d25ddb718a27
matheusschwertz <matheusschwertz@gmail.com>
2023-08-20 02:03:05 -0300

    fix: formating according flake8

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return {"massage": "user 1"}
 
+
 class User(Resource):
     def post(self):
         return {"massage": "teste"}
-    
+
     def get(serf, cpf):
-        return {"massage":"CPF"}
-    
+        return {"massage": "CPF"}
+
 
 api.add_resource(Users, "/users")
 api.add_resource(User, "/user", "/user/<string:cpf>")
