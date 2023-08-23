50084b3c1977deaaff3c7997d3d214346efdd75f
matheusschwertz <matheusschwertz@gmail.com>
2023-08-23 09:01:02 -0300

    feat: reqparse

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_mongoengine import MongoEngine
 
 
@@ -13,6 +13,34 @@ app.config['MONGODB_SETTINGS'] = {
     'password': 'admin'
 }
 
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('first_name',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('last_name',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('cpf',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('email',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('birth_date',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+
+
 api = Api(app)
 db = MongoEngine(app)
 
@@ -22,7 +50,7 @@ class UserModel(db.Document):
     first_name = db.StringField(required=True)
     last_name = db.StringField(required=True)
     email = db.EmailField(required=True)
-    bith_date = db.DateTimeField(required=True)
+    birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
@@ -32,7 +60,8 @@ class Users(Resource):
 
 class User(Resource):
     def post(self):
-        return {"massage": "teste"}
+        data = _user_parser.parse_args()
+        UserModel(**data).save()
 
     def get(serf, cpf):
         return {"massage": "CPF"}
